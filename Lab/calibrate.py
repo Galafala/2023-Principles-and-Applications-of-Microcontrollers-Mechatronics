a55b0efadd6f339ac2c12b58437e6454f7107835
Tzu-Ching Kuo <ben100719@gmail.com>
2023-04-27 23:12:19 +0800

    modified calibrate.py, calculate k1, k2 with multiple times averages

--- Lab/calibrate.py
+++ Lab/calibrate.py
@@ -11,14 +11,13 @@ def generate_random(length):
     import random
 
     index = list(np.arange(0,length,1))
-    x = random.sample(y, int(length/2))
-    x.sort()
-    y = []
+    random.shuffle(index)
 
-    for i in index:
-        if i in x:
-            continue
-        y.append(i)
+    x = index[:int(length/2)]
+    y = index[int(length/2):]
+
+    x.sort()
+    y.sort()
     
     return x, y
  
@@ -30,52 +29,59 @@ plt.xlabel('distance [cm]')
 plt.ylabel('voltage [V]')
 plt.savefig('general_shape.png')
 
-sample1, sample2 = generate_random(len(distances))
-distances_trimed=distances[sample1]
-raw_measurements_total_trimed=raw_measurements_total[sample1]
- 
-# validation data 
-distances2=distances[sample2]
-raw_measurements2=raw_measurements_total[sample2]
- 
-print(distances_trimed)
-print(distances2)
-n=distances_trimed.shape[0]
-y=np.zeros(shape=(n,1))
-A=np.zeros(shape=(n,2))
- 
-for i in range(n):
-    y[i,0]=np.log(raw_measurements_total_trimed[i])
-    A[i,0]=1
-    A[i,1]=np.log(distances_trimed[i])
- 
-# solution    
-c=np.matmul(np.matmul(np.linalg.inv(np.matmul(A.T,A)),A.T),y)
-k1=np.exp(c[0])
-k2=c[1]
- 
-# test on the estimation data
-raw_measurements_total_trimed_prediction=np.zeros(shape=(n,1))
-for i in range(n):
-    raw_measurements_total_trimed_prediction[i]=k1*distances_trimed[i]**(k2)
- 
-plt.plot(distances_trimed,raw_measurements_total_trimed_prediction,'xr',label='least-squares prediction')
-plt.plot(distances_trimed,raw_measurements_total_trimed,'k',label='real data')
-plt.xlabel('distance [cm]')
-plt.ylabel('voltage [V]')
-plt.legend()
-plt.savefig('estimation_curve.png')
- 
-# test on the validation data 
-n1=distances2.shape[0]
-raw_measurements2_prediction=np.zeros(shape=(n1,1))
-for i in range(n1):
-    raw_measurements2_prediction[i]=k1*distances2[i]**(k2)
+k1 = 0
+k2 = 0
+
+times = 5000
+for _ in range(times):
+    sample1, sample2 = generate_random(len(distances))
+    distances_trimed=distances[sample1]
+    raw_measurements_total_trimed=raw_measurements_total[sample1]
     
+    # validation data 
+    distances2=distances[sample2]
+    raw_measurements2=raw_measurements_total[sample2]
+    
+    # print(distances_trimed)
+    # print(distances2)
+    n=distances_trimed.shape[0]
+    y=np.zeros(shape=(n,1))
+    A=np.zeros(shape=(n,2))
+    
+    for i in range(n):
+        y[i,0]=np.log(raw_measurements_total_trimed[i])
+        A[i,0]=1
+        A[i,1]=np.log(distances_trimed[i])
+    
+    # solution    
+    c=np.matmul(np.matmul(np.linalg.inv(np.matmul(A.T,A)),A.T),y)
+    k1+=np.exp(c[0])
+    k2+=c[1]
+    
+    # test on the estimation data
+    # raw_measurements_total_trimed_prediction=np.zeros(shape=(n,1))
+    # for i in range(n):
+    #     raw_measurements_total_trimed_prediction[i]=k1*distances_trimed[i]**(k2)
+    
+    # plt.plot(distances_trimed,raw_measurements_total_trimed_prediction,'xr',label='least-squares prediction')
+    # plt.plot(distances_trimed,raw_measurements_total_trimed,'k',label='real data')
+    # plt.xlabel('distance [cm]')
+    # plt.ylabel('voltage [V]')
+    # plt.legend()
+    # plt.savefig('estimation_curve.png')
+    
+    # test on the validation data 
+    # n1=distances2.shape[0]
+    # raw_measurements2_prediction=np.zeros(shape=(n1,1))
+    # for i in range(n1):
+    #     raw_measurements2_prediction[i]=k1*distances2[i]**(k2)
+
+k1 /= times
+k2 /= times
 print(k1, k2)
-plt.plot(distances2,raw_measurements2_prediction,'xr',label='least-squares prediction')
-plt.plot(distances2,raw_measurements2,'k',label='real data')
-plt.xlabel('distance [cm]')
-plt.ylabel('voltage [V]')
-plt.legend()
-plt.savefig('validation_curve.png')
+    # plt.plot(distances2,raw_measurements2_prediction,'xr',label='least-squares prediction')
+    # plt.plot(distances2,raw_measurements2,'k',label='real data')
+    # plt.xlabel('distance [cm]')
+    # plt.ylabel('voltage [V]')
+    # plt.legend()
+    # plt.savefig('validation_curve.png')
